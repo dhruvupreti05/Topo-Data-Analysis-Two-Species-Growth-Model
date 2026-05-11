87a821269bd4cab9cb7c83924e8d82750f5e62ae
Dhruv Upreti <121254938+dhruvupreti05@users.noreply.github.com>
2026-05-11 10:30:25 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,13 @@ set +a
 
 git remote add overleaf https://git.overleaf.com/$OVERLEAF_ID
 
+if ! git remote | grep -q 'overleaf'; then
+    git remote add overleaf "https://git.overleaf.com/$OVERLEAF_ID"
+    echo "Added Overleaf remote."
+else
+    echo "Overleaf remote exists"
+fi
+
 python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
@@ -15,5 +22,4 @@ python -m ipykernel install --user --name=repo-env --display-name "Repository En
 
 echo ""
 echo "Setup complete."
-echo "Activate the environment with:"
-echo "source ocean/bin/activate"
+echo "Activate the environment with: `source .env/bin/activate`"
