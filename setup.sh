d54afe9314de05b16c8fd528a58d7958e509a59b
Dhruv Upreti <121254938+dhruvupreti05@users.noreply.github.com>
2026-05-11 10:14:44 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ python3 -m venv .venv
 source ocean/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
+python -m ipykernel install --user --name=repo-env --display-name "Repository Environment"
 
 echo ""
 echo "Setup complete."
